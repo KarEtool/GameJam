608afa40f8e3f0f9e89ddd9e1fb74d18ab7095fb
YigitYeler <74016134+YigitYeler@users.noreply.github.com>
2021-02-13 00:01:52 +0300

    Fix2

--- New Unity Project/Assets/Scripts/CameraMenager.cs
+++ New Unity Project/Assets/Scripts/CameraMenager.cs
@@ -15,6 +15,6 @@ public class CameraMenager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Slerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), cameraSpeed);
+        transform.position = Vector3.Slerp(transform.position, new Vector3(target.position.x, 3.37f, transform.position.z), cameraSpeed);
     }
 }
