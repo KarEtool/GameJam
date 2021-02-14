436225608dbf11dc86a84db80171b25e83352cb7
cicimma <76131730+cicimma@users.noreply.github.com>
2021-02-14 18:21:40 +0300

    Ü

---
+++ New Unity Project/Assets/Scripts/BOSSKAPI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BOSSKAPI : MonoBehaviour
+{
+    public GameObject bosstextBu;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            bosstextBu.SetActive(true);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    { 
+        if ((other.tag == "Player") && Input.GetKeyDown("f")) 
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            bosstextBu.SetActive(false);
+        }
+    }
+}
